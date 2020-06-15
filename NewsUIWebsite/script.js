0bf7812b376f52917e63515e8aa0484f0c030515
Tanase Nicoleta <62150787+TanaseNicoleta@users.noreply.github.com>
2020-06-15 16:12:52 +0300

    Add files via upload

---
+++ NewsUIWebsite/script.js
@@ -0,0 +1,15 @@
+
+function myFunction() 
+{
+    var click = document.getElementById("elemMeniu");
+    if(click.style.display == "none")
+        click.style.display = "block";
+    else
+        click.style.display = "none"; 
+  
+} 
+
+
+
+
+
